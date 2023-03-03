961990f9869b569f3ad3003549f133e9b389198e
ShafiArnob <shafiarnob@gmail.com>
2023-03-03 08:19:09 +0600

    room connection established

--- client/src/App.js
+++ client/src/App.js
@@ -11,26 +11,14 @@ function App() {
 
   const joinRoom = () => {
     if(username!=="" && room!==""){
-      
+      socket.emit("join_room", room)
     }
   }
   return (
     <div className="App">
       <h3>Join A Chat</h3>
-          <input
-            type="text"
-            placeholder="John..."
-            onChange={(event) => {
-              setUsername(event.target.value);
-            }}
-          />
-          <input
-            type="text"
-            placeholder="Room ID..."
-            onChange={(event) => {
-              setRoom(event.target.value);
-            }}
-          />
+          <input type="text" placeholder="John..." onChange={(event) => {setUsername(event.target.value);}}/>
+          <input  type="text"  placeholder="Room ID..." onChange={(event) => {setRoom(event.target.value);}}/>
           <button onClick={joinRoom}>Join A Room</button>
     </div>
   );
