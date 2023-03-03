8c1ac732f5c591e3a564886af985ba0faa4cf842
ShafiArnob <shafiarnob@gmail.com>
2023-03-03 08:13:27 +0600

    room connection page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client'
+import { useState } from 'react';
+
+const socket = io.connect("http://localhost:4000")
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if(username!=="" && room!==""){
+      
+    }
+  }
   return (
     <div className="App">
-      <h1>APP Init</h1>
+      <h3>Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="John..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
     </div>
   );
 }
